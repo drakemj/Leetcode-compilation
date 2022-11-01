fe487ec208613a0413c51727792c99e519553bf0
drakemj <drakemj@uci.edu>
2022-10-31 23:55:02 -0700

    added comments to kth smallest element

--- KthSmallestElement.cpp
+++ KthSmallestElement.cpp
@@ -19,17 +19,17 @@ public:
     int kthSmallest(TreeNode* root, int k) {
         int out = 0;
         if (root->left) {
-            out = kthSmallest(root->left, k);
-            if (out >= 0) return out;
-            if (out + k == 1) return root->val;
+            out = kthSmallest(root->left, k);                       //continue left to reach the smallest.
+            if (out >= 0) return out;                               //out returns a negative value corresponding to the amount of smallest nodes checked.
+            if (out + k == 1) return root->val;                     //once control returns here, can check this node, else go right.
         }
-        if (k == 1) return root->val;
+        if (k == 1) return root->val;                               //right side can cause k to equal 1.
         if (root->right){
-            int t = kthSmallest(root->right, k + out - 1);
+            int t = kthSmallest(root->right, k + out - 1);          //must immediately check for left nodes first, keep track of out accumulated from left
             if (t > 0) return t;
             out += t;
         }
-        return out-1;
+        return out-1;                                               //return control to node if left and right have been checked, pass out
     }
 
 };
